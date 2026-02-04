d3037119d02b0c233790a6aa25d31cc9ca4d9d3f
rivon0507 <flavienrivon@gmail.com>
2026-02-04 13:20:14 +0300

    test(auth): add logout tests for MainLayout

--- src/app/layout/main/main.layout.spec.ts
+++ src/app/layout/main/main.layout.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { MainLayout } from './main.layout';
 import { mockMatchMedia } from '@test';
+import { AuthApi } from "@core/api/auth.api";
+import { AuthApiMock } from '@core/api/auth.api.mock';
+import { By } from '@angular/platform-browser';
+import { SessionStore } from '@core/session/session.store';
 
 describe('MainLayout', () => {
   let component: MainLayout;
   let fixture: ComponentFixture<MainLayout>;
+  let router: Router;
+  let sessionStore: SessionStore;
 
   beforeEach(() => {
     if (!window.matchMedia) {
@@ -17,12 +23,17 @@ describe('MainLayout', () => {
 
     TestBed.configureTestingModule({
       imports: [MainLayout],
-      providers: [provideRouter([])]
+      providers: [
+        provideRouter([]),
+        {provide: AuthApi, useClass: AuthApiMock},
+      ]
     });
 
     fixture = TestBed.createComponent(MainLayout);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+    sessionStore = TestBed.inject(SessionStore);
   });
 
   it('should compile', () => {
@@ -33,5 +44,22 @@ describe('MainLayout', () => {
     const element = fixture.nativeElement as HTMLElement;
     expect(element.querySelector('mat-sidenav-container')).not.toBeNull();
     expect(element.querySelector('router-outlet')).not.toBeNull();
+  });
+
+  it('should navigate to login page on logout button click', () => {
+    const navigateByUrlSpy = vi.spyOn(router, "navigateByUrl");
+    const logoutButton = fixture.debugElement.query(By.css("mat-nav-list button")).nativeElement as HTMLButtonElement;
+    expect(logoutButton).toBeTruthy();
+    logoutButton.click();
+    expect(navigateByUrlSpy).toHaveBeenCalledExactlyOnceWith("/auth/login")
+  });
+
+  it('should clear session state on logout', () => {
+    const logoutButton = fixture.debugElement.query(By.css("mat-nav-list button")).nativeElement as HTMLButtonElement;
+    expect(logoutButton).toBeTruthy();
+    logoutButton.click();
+    fixture.detectChanges();
+    expect(sessionStore.isAuthenticated()).toBeFalsy();
+    expect(sessionStore.user()).toBeNull();
   });
 });
